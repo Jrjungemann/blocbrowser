0585b3e22282d836b2c9b7826d24f0d02cd82e73
Jonathan Jungemann <jrjungemann@gmail.com>
2014-10-05 13:04:58 -0500

    Initial Commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Jonathan Jungemann on 10/5/14.
+//  Copyright (c) 2014 Jon Jungemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
